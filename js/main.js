e6067cf9105afefa8385555e580c0531281120bb
teogen404 <gartungaleksandr4@gmail.com>
2022-11-17 16:51:54 +0100

    123

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@ function openMenu() {
     document.querySelector('.menu__button').classList.toggle('active');
     document.querySelector('body').classList.toggle('active');
 }
+function nabidky(){
+    document.location = '/nabidky.html';
+}
+function zivotopisy(){
+    document.location = '/zivotopisy.html';
+}
+function sluzby(){
+    document.location = '/sluzby.html';
+}
+function p__mista(){
+    document.location = '/p-mista.html';
+}
