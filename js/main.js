0b7808157b4b182ab1033ad017977a0cf573a977
teogen404 <gartungaleksandr4@gmail.com>
2022-12-04 17:29:34 +0100

    123

--- js/main.js
+++ js/main.js
@@ -3,18 +3,6 @@ function openMenu() {
     document.querySelector('.menu__button').classList.toggle('active');
     document.querySelector('body').classList.toggle('active');
 }
-function nabidky(){
-    document.location = '/nabidky.html';
-}
-function zivotopisy(){
-    document.location = '/zivotopisy.html';
-}
-function sluzby(){
-    document.location = '/sluzby.html';
-}
-function p__mista(){
-    document.location = '/p-mista.html';
-}
 
 function openFAQ(){
     var faq = document.getElementsByClassName("question");
